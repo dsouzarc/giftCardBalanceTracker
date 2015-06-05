488d2f59da944c5d3c4dee1f72e6634ff407c6c6
Ryan D'souza <dsouzarc@gmail.com>
2015-06-05 06:12:34 -0400

    Ignores .html file which contains sample data

---
+++ Gift Card Balance Tracker/AllCardsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AllCardsViewController.h
+//  Gift Card Balance Tracker
+//
+//  Created by Ryan D'souza on 6/5/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface AllCardsViewController : ViewController
+
+@end
---
+++ Gift Card Balance Tracker/AllCardsViewController.m
@@ -0,0 +1,37 @@
+//
+//  AllCardsViewController.m
+//  Gift Card Balance Tracker
+//
+//  Created by Ryan D'souza on 6/5/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "AllCardsViewController.h"
+
+@interface AllCardsViewController ()
+
+@end
+
+@implementation AllCardsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
